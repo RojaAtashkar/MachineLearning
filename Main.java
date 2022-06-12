2e7271f23a48d93c5940fd0fd59163a6bca81361
Amin Ansari <aminansari80@gmail.com>
2022-06-12 22:24:10 +0430

    ready data structure

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public HashMap<String,Integer> polarityOnes(){
+        HashMap<String,Integer> ones = new HashMap<>();
+
+        return  ones;
+    }
+
+    public HashMap<String,Integer> polarityZeroes(){
+        HashMap<String,Integer> zeroes = new HashMap<>();
+
+        return  zeroes;
+    }
+}
